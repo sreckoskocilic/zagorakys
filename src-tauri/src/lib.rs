81ab9ef5070d92c2418f759eb93d2f17e999cde4
Srecko Skocilic <srecko.skocilic@gmail.com>
2026-05-13 07:53:29 +0200

    workflow and cleanup

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,7 +5,6 @@ use converter::{convert_comic, get_mobi_info, get_mobi_page};
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![
             get_mobi_info,
